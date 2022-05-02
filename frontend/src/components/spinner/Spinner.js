e6c23c876d458c427b8b0050ca396d6fb66e58cb
obaro89 <igbinobaroosaretin@gmail.com>
2022-05-02 17:13:01 +0100

    updated the ui

--- frontend/src/components/spinner/Spinner.js
+++ frontend/src/components/spinner/Spinner.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { SpinnerRoundFilled } from "spinners-react";
+import { SpinnerCircular } from "spinners-react";
 import "./spinner.scss";
 
 const Spinner = () => {
   return (
     <div className="loading">
-      <SpinnerRoundFilled color="#aa54db" size="150px" thickness={300} />
+      <SpinnerCircular color="#828282" size="150px" thickness={300} />
     </div>
   );
 };
